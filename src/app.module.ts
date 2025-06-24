fac3c2bf46f3384428c66f20acc7379bbda1e26b
Ihor Kosandiak <ihor.kosandyak@oril.co>
2025-06-24 12:09:09 +0300

    WIP: small chyanges

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env.development.local', '.env.development', '.env'],
+      isGlobal: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
